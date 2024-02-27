b23527e5a0b250c622ff0f01fd18019fa37ed29c
kukuhtri1999 <kukuhthewow@gmail.com>
2024-02-27 23:05:49 +0700

    fixing index and add jsonwebtoken feature

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // src/index.js
 const express = require('express')
+const bodyParser = require('body-parser')
 const { specs, swaggerUi } = require('./swagger/swagger')
 const app = express()
 const PORT = process.env.PORT || 3000
+const authRoutes = require('./routes/auth')
+
+app.use(bodyParser.json())
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello, Operate360!')
